10d6fa6ba263e26678c1aedd6eac348c2e2ad2db
Rafael Maia <rafaelmfcontato@protonmail.com>
2026-02-06 15:21:21 -0300

    feat: add `env.ts` and `config.ts` with `zod` validation

---
+++ src/config/config.ts
@@ -0,0 +1,25 @@
+import { env } from './env';
+
+export const config = {
+  app: {
+    env: env.APP_ENV,
+    port: env.PORT,
+    isProduction: env.APP_ENV === 'production',
+  },
+
+  database: {
+    host: env.POSTGRES_HOST,
+    port: env.POSTGRES_PORT,
+    user: env.POSTGRES_USER,
+    password: env.POSTGRES_PASSWORD,
+    name: env.POSTGRES_DB,
+
+    url: `postgresql://${env.POSTGRES_USER}:${env.POSTGRES_PASSWORD}@${env.POSTGRES_HOST}:${env.POSTGRES_PORT}/${env.POSTGRES_DB}`,
+  },
+  redis: {
+    host: env.REDIS_HOST,
+    port: env.REDIS_PORT,
+
+    url: `redis://${env.REDIS_HOST}:${env.REDIS_PORT}`,
+  },
+};
---
+++ src/config/env.ts
@@ -0,0 +1,35 @@
+import dotenv from 'dotenv';
+import { z } from 'zod';
+
+const appEnv = process.env.APP_ENV ?? 'development';
+
+dotenv.config({
+  path: `.env.${appEnv}`,
+});
+
+const envSchema = z.object({
+  APP_ENV: z.enum(['development', 'test', 'production']).default('development'),
+
+  PORT: z.coerce.number().int().positive(),
+
+  POSTGRES_HOST: z.string(),
+  POSTGRES_PORT: z.coerce.number().int().positive(),
+  POSTGRES_USER: z.string(),
+  POSTGRES_PASSWORD: z.string(),
+  POSTGRES_DB: z.string(),
+
+  REDIS_HOST: z.string(),
+  REDIS_PORT: z.coerce.number().int().positive(),
+});
+
+const parsedEnv = envSchema.safeParse(process.env);
+
+if (!parsedEnv.success) {
+  console.error(
+    '❌ Invalid environment variables:',
+    z.treeifyError(parsedEnv.error),
+  );
+  process.exit(1);
+}
+
+export const env = parsedEnv.data;
